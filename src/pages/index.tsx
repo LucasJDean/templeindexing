391b16b18c5ed7cf5902132732e0a53c87c35ef0
LucasJDean <2341301556@k12.hi.us>
2024-01-08 14:31:55 -1000

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/post/first-post"
+              href="src\pages\posts\first-post"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
