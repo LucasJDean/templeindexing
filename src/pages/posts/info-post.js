3a3b54c39d234cc1dbbaea53f6ebf82d9b46d83d
LucasJDean <2341301556@k12.hi.us>
2024-01-25 13:16:42 -1000

    routing pages add

---
+++ src/pages/posts/info-post.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Link from "next/link";
+
+
+export default function InfoPost() {
+    return (
+      <>
+        <h1>Info</h1>
+        <h2>
+          <Link href="/">Back to home page</Link>
+        </h2>
+      </>
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/pages/posts/search-post.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+
+
+export default function SearchPost() {
+    return (
+      <>
+        <h1>Search DataBase</h1> 
+        <h2>
+          <Link href="/">Back to home page</Link>
+        </h2>
+      </>
+    );
+  }
+
