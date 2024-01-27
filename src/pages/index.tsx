b334fa67a04a5d0f55813ffb65fae5a8c065fea2
LucasJDean <2341301556@k12.hi.us>
2024-01-26 19:51:25 -1000

    position change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
   </Link>
 </div>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#505250] to-[#CBD3C1]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-7 py-10 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-gray sm:text-[5rem]">
             Were Making It <span className="text-black">F͟u͟n</span> Again.
           </h1>
