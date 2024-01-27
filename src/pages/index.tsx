ac6cb5b73305a34e3d94d9a08d729d54fb9ca12a
LucasJDean <2341301556@k12.hi.us>
2024-01-26 17:35:59 -1000

    navbar links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <li><a>Homepage</a></li>
       </Link>
       <Link
-        href="/pages/leader-post.js"
+        href="/posts/leader-post.js"
       >
       <li><a>Leaderboard</a></li>
       </Link>
@@ -46,14 +46,18 @@ export default function Home() {
       </ul>
     </div>
   </div>
+  <Link
+  href="/"
+  target="_blank">
   <div className="navbar-center">
     <a className="btn btn-ghost text-xl">The Temple Of Indexing</a>
-</div>
+  </div>
+  </Link>
 </div>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#505250] to-[#CBD3C1]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-gray sm:text-[5rem]">
-            Were Making It <span className="text-black">Fun</span> Again.
+            Were Making It <span className="text-black">F͟u͟n͟</span> Again.
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
