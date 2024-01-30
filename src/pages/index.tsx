a8f5db1f5cb77d8a2833d1b4cc31c53dbbdb93f5
LucasJDean <2341301556@k12.hi.us>
2024-01-29 14:31:53 -1000

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,10 +61,18 @@ export default function Home() {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           </div>
-          <h3 className="text-2xl font-bold">About →</h3>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/posts/info-post"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">About →</h3>
               <div className="text-lg">
-                Learn more about how this all started.
+                Learn more about how this started.
               </div>
+            </Link>
+          </div>  
           <p className="text-2xl text-black">
             {hello.data ? hello.data.greeting : "Loading..."}
           </p>
