ff91e0b4a5cfd3b13cb06d93e0f24b280c157d69
LucasJDean <2341301556@k12.hi.us>
2024-02-08 13:34:11 -1000

    mongodb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,17 +57,6 @@ export default function Home() {
    </div>
    </Link>
    </div>
-   <div className="navbar-end">
-    <button className="btn btn-ghost btn-circle">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-    <button className="btn btn-ghost btn-circle">
-      <div className="indicator">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-        <span className="badge badge-xs badge-primary indicator-item"></span>
-      </div>
-    </button>
-   </div>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#505250] to-[#CBD3C1]">
         <div className="container flex flex-col items-center justify-center gap-12 px-7 py17 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-gray sm:text-[5rem]">
@@ -76,7 +65,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           </div>
             {/*Login Stuff*/}      
-            <div className="">
+          <div className="">
             {/*Email and Username*/}
             <input type="text" placeholder="Email or Username" className="input input-bordered w-full max-w-xs" />
             {/*Password*/}
@@ -90,7 +79,7 @@ export default function Home() {
                 <button className="btn neutral">Sign Up</button>
               </Grid>
             </Grid>
-            </div>
+          </div>
               <p className="text-2xl text-black">
                 {hello.data ? hello.data.greeting : "Loading..."}
               </p>    
