80446266a7812a175e5bf0935cb72306bcc45fd0
LucasJDean <2341301556@k12.hi.us>
2024-03-11 14:29:48 -1000

    asdf

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,15 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+const database = 'grades';
+const collection = 'testscores';
 
-const prisma = new PrismaClient();
+use(database);
 
-async function main() {
-  const post = await prisma.post.create({
-    data: {
-      title: 'My first post',
-      body: 'My first post body'
-    }
-  });
-  console.log(post);
-}
-
-main();
+db.createCollection(collection);
